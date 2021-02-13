2c685902108bf3417de9941df091ef9f28d377ec
pandisoft <pandisoft.com@gmail.com>
2021-02-13 15:57:59 +0100

    create blade files set scss and vuejs files Remove some default files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('base', ['pageTitle' => 'NextMedia Coding challenge - Homepage'])
+
+@section('main_content')
+    <header>
+        <nav class="bg-white shadow-lg mb-8">
+            <div class="md:flex items-center justify-between container mx-auto">
+                <div class="flex justify-between items-center">
+                    <div class="text-2xl font-bold text-gray-600 md:text-3xl p-5">
+                        <a href="#">NextMedia Coding challenge</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="main container mx-auto">
+        
+    </main>
+</div>
+@endsection
